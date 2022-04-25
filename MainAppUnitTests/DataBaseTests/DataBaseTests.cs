f0d1850dd011f07e46679b8ce805c8af11abbee0
Mariusz Malec <mario106c@gmail.com>
2022-04-25 18:05:11 +0200

    fixed unit test delete database before test and create new one

--- MainAppUnitTests/DataBaseTests/DataBaseTests.cs
+++ MainAppUnitTests/DataBaseTests/DataBaseTests.cs
@@ -11,7 +11,7 @@ namespace MainAppUnitTests.DataBaseTests
     public class DataBaseTests
     {   
         [Fact]
-        public void CheckDataBase_ReturnError_WhenNotUsersExist()
+        public void CheckDataBase_ReturnError_WhenNotTrainersExist()
         {
             var connection = new SqliteConnection("Data Source=C:\\temp\\Databases\\MainAppDb.db");
             connection.Open();
@@ -24,9 +24,18 @@ namespace MainAppUnitTests.DataBaseTests
 
                 using (var context = new MainApplicationContext(options))
                 {
+                    connection.Close();
+
+                    //delete before test
+                    context.Database.EnsureDeleted();
+
+                    connection.Open();
+
+                    //create again
                     context.Database.EnsureCreated();
+
                     //init data
-                    var any = context.Users.AnyAsync();
+                    var any = context.Trainers.AnyAsync();
                     //assert
                     any.Result.Should().Be(true);
                 }
@@ -52,9 +61,19 @@ namespace MainAppUnitTests.DataBaseTests
                 using (var context = new MainApplicationContext(options))
                 {
                     var userId = 104;
+
+                    connection.Close();
+
+                    //delete before test
+                    context.Database.EnsureDeleted();
+
+                    connection.Open();
+
+                    //create again
                     context.Database.EnsureCreated();
+
                     //init data
-                    context.Trainers.Add(new Trainer { Id = userId, FirstName = "trlalal", LastName = "bebeb", CreatedDate=DateTime.Now, Email = "cepek@example.com", PhoneNumber = "222-222-222"});
+                    context.Trainers.Add(new Trainer { Id = userId, FirstName = "Test", LastName = "Jednostkowy", CreatedDate=DateTime.Now, Email = "cepek@example.com", PhoneNumber = "222-222-222"});
                     
                     //Act
                     Action act = () => context.SaveChanges();
