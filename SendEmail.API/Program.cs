9dd3c29dcbb34f8ac92ac70ee3a2f1634737827c
Mariusz Malec <mario106c@gmail.com>
2022-06-09 22:25:38 +0200

    added httpclientfactory

--- SendEmail.API/Program.cs
+++ SendEmail.API/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IEmailService, EmailService>();
+builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
--- SendEmail.API/Services/EmailService.cs
+++ SendEmail.API/Services/EmailService.cs
@@ -9,23 +9,39 @@ namespace SendEmail.API.Services
     public class EmailService : IEmailService
     {
         private readonly IConfiguration _config;
-        public EmailService(IConfiguration config)
+        IHttpClientFactory _httpClientFactory;
+        public EmailService(IConfiguration config, IHttpClientFactory httpClientFactory)
         {
             _config = config;
+            _httpClientFactory = httpClientFactory;
         }
 
         public IConfiguration Config { get; }
 
         public async Task SendEmail(EmailDto request)
         {
+            //Use SimpleMailTransferProtocol
+            HttpClient client = _httpClientFactory.CreateClient();//dodane aby wyciagnac dane z tracking api
+
             var email = new MimeMessage();
             email.From.Add(MailboxAddress.Parse(_config.GetSection("EmailUserName").Value));
             email.To.Add(MailboxAddress.Parse(request.To));
             email.Subject = request.Subject;
             email.Body = new TextPart(TextFormat.Html) { Text = request.Body };
 
-            using var smtp = new SmtpClient();//remember use reference MailKit.Net.Smtp!
+            using var smtp = new SmtpClient();//remember use reference MailKit.Net.Smtp! More safetly!!
+
+            //jak zrobic np z 2-verification bo teraz jest malo bezpiecznie!
+
             smtp.Connect(_config.GetSection("EmailHost").Value, 587, SecureSocketOptions.StartTls);//smtp.gmail.com , smtp.office365.com
+
+            var authenticate = smtp.IsAuthenticated;
+
+            if (!authenticate)
+            {
+                throw new Exception($"Brak autoryzacji!! BoxEmail is securited!! Go to google account, select less security option and switch on it");
+            }
+
             smtp.Authenticate(_config.GetSection("EmailUserName").Value, _config.GetSection("EmailPassword").Value);
             await smtp.SendAsync(email);
             smtp.Disconnect(true);
