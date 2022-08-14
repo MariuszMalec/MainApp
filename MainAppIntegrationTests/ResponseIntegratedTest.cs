945e5aac50537f5ea990195ba9582421f9385879
Mariusz Malec <mario106c@gmail.com>
2022-08-14 15:30:40 +0200

    add new integrated test to ResponseIntegratedTest

--- MainAppIntegrationTests/ResponseIntegratedTest.cs
+++ MainAppIntegrationTests/ResponseIntegratedTest.cs
@@ -1,6 +1,11 @@
 ﻿using FluentAssertions;
+using MainApp.BLL.Entities;
+using Newtonsoft.Json;
+using System;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
 using Tracking;
 using Xunit;
@@ -16,7 +21,7 @@ namespace MainAppIntegrationTests
             => _client = factory.CreateClient();
 
         [Fact]
-        public async Task Index_ReturnsWrongResponse_WhenIsNotStatusOK()
+        public async Task Index_ReturnsTrainersWrongResponse_WhenIsNotStatusOK()
         {
             // Arrange
             var response = await _client.GetAsync($"{AppiUrl}/Trainer");//TODO zmien na enpoint ktorego nie ma w api aby wywalilo test
@@ -24,6 +29,35 @@ namespace MainAppIntegrationTests
 
             // Act
             var responseString = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task Create_Trainer_ReturnWrongResponse_WhenIsNotStatusOK()
+        {
+            // Arrange
+            var trainer = new Trainer
+            {
+                Id = 1,
+                CreatedDate = DateTime.Now,
+                FirstName = "Piotr",
+                LastName =  "Grot",
+                Email = "pg@example.com",
+                PhoneNumber = "505859599"
+                
+            };
+
+            var request = new HttpRequestMessage(HttpMethod.Post, $"{AppiUrl}/Trainer/Create");
+
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            request.Content = new StringContent(JsonConvert.SerializeObject(trainer), Encoding.UTF8, "application/json");
+
+            // Act
+            var response = await _client.SendAsync(request);
+
 
             // Assert
             response.StatusCode.Should().Be(HttpStatusCode.OK);
