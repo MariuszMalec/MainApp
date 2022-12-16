11b71c792e3d1acc6cb212bce208cb93fd0b52d0
Mariusz Malec <mario106c@gmail.com>
2022-12-16 15:51:34 +0100

    add some integrated test authirozed with apikey

---
+++ MainAppIntegrationTests/IntegrationTrainerTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Tracking;
+using Xunit;
+
+namespace MainAppIntegrationTests
+{
+    public class IntegrationTrainerTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IConfigurationSection _authSettings;
+
+        public IntegrationTrainerTests(WebApplicationFactory<Startup> factory)
+        {
+            _httpClient = factory.CreateClient();
+
+            _authSettings = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appsettings.json")
+               .Build()
+               .GetSection("ApiKey");
+        }
+
+        [Fact]
+        public async Task GetAll_Trainers_WithAuthenticateApiKey_StatusOk()
+        {
+            // Arrange
+            var request = new HttpRequestMessage(HttpMethod.Get, "/api/Trainer");
+
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            request.Headers.Add("ApiKey", _authSettings.Value);//TODO Apikey do headera
+
+            // Act
+            var result = await _httpClient.SendAsync(request);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetAll_Trainers_WithOutAuthenticateApiKey_StausUnauthorized()
+        {
+            // Arrange
+            var request = new HttpRequestMessage(HttpMethod.Get, "/api/Trainer");
+
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            // Act
+            var result = await _httpClient.SendAsync(request);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Unauthorized, result.StatusCode);
+        }
+    }
+
+}
+
--- MainAppIntegrationTests/TrainerControllerTests.cs
+++ MainAppIntegrationTests/TrainerControllerTests.cs
@@ -1,31 +1,83 @@
 ﻿using FluentAssertions;
+using FluentAssertions.Common;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Net.Http.Headers;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Tracking;
+using Tracking.Authentication.ApiKey;
 using Xunit;
 
 namespace MainAppIntegrationTests
 {
     public class TrainerControllerTests : IClassFixture<WebApplicationFactory<Startup>>//wspoldzielenie factory testy nieco szybsze
     {
-        private HttpClient _client;
+        private IConfigurationSection _authSettings;
+        private HttpClient _httpClient;
 
         public TrainerControllerTests(WebApplicationFactory<Startup> factory)
         {
             //https://youtu.be/6keSabBQRdE?t=2665
-            _client = factory.CreateClient();//to dziala ale musi byc polaczenie z baza
+            _authSettings = new ConfigurationBuilder()
+           .SetBasePath(Directory.GetCurrentDirectory())
+           .AddJsonFile("appsettings.json")
+           .Build()
+           .GetSection("ApiKey");
+
+            _httpClient = factory.CreateClient();
+            _httpClient.BaseAddress = new Uri("https://localhost:7001/");
+            _httpClient.Timeout = new TimeSpan(0, 0, 30);
+            _httpClient.DefaultRequestHeaders.Add(
+                HeaderNames.Accept, "application/json");
+            _httpClient.DefaultRequestHeaders.Add("ApiKey", _authSettings.Value);
         }
 
         [Fact]
-        public async Task GetAll_Trainers_ReturnOk_WhenExist()
+        public async Task Get_TrainerWithApiKeyAuthentication_ReturnStatusOk()
         {
+            //arrange
 
             //act
-            var response = await _client.GetAsync("/api/Trainer");
+            var response = await _httpClient.GetAsync("/api/Trainer/1");
 
             //assert
-            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
         }
+
+        [Fact]
+        public async Task Get_TrainerWithApiKeyAuthentication_ReturnStatusNotFound()
+        {
+            //arrange
+
+            //act
+            var response = await _httpClient.GetAsync("/api/Trainer/100");
+
+            //assert
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+
+
+        [Fact]
+        public async Task GetAll_Trainers_WithApiKeyAuthentication_ReturnStatusOk()
+        {
+            // Arrange
+            var expectedStatusCode = HttpStatusCode.OK;
+            var request = new HttpRequestMessage(HttpMethod.Get, $"/api/Trainer");
+
+            //Act
+            var response = await _httpClient.SendAsync(request);
+
+            // Assert
+            Assert.Equal(expectedStatusCode, response.StatusCode);
+        }
+
     }
 }
