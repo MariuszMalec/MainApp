454d42a943f5c36820be194b7e0d62fb36b50246
Mariusz Malec <mario106c@gmail.com>
2023-02-27 21:01:09 +0100

    add provider to home controller

--- MainApp.Web/Controllers/HomeController.cs
+++ MainApp.Web/Controllers/HomeController.cs
@@ -1,13 +1,19 @@
-﻿using MainApp.BLL.Models;
+﻿using MainApp.BLL.Context;
+using MainApp.BLL.Enums;
+using MainApp.BLL.Models;
 using MainApp.Web.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.DotNet.Scaffolding.Shared;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using QC = Microsoft.Data.SqlClient;
 
 namespace MainApp.Web.Controllers
 {
@@ -16,15 +22,46 @@ namespace MainApp.Web.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IConfiguration _configuration;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public IActionResult Index(LoginView model)
         {
+            var defaultprovider = _configuration["DatabaseProvider"];
+            if (model.ProviderName == null)
+                model.ProviderName = defaultprovider;
             var provider = model.ProviderName;
             return View(model);
+        }
+
+        public IActionResult Edit(LoginView model)
+        {
+            int intProvider = Convert.ToInt32(model.ProviderName);
+            string strProvider = Enum.GetName(typeof(Provider), intProvider);
+
+            var connectionString = _configuration.GetConnectionString(strProvider);
+
+
+            //using (var connection = new QC.SqlConnection(
+            //        connectionString
+            //    ))
+            //{
+            //    connection.Open();
+            //    _logger.LogWarning($"Connected to {strProvider} successfully.");
+            //    //return RedirectToAction("Index");
+
+            
+
+
+
+            _logger.LogWarning($"Connected to {strProvider} successfully.");
+
+            return RedirectToAction("Index", model);
         }
 
         public IActionResult Privacy()
