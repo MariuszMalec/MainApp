7a40a0a667e3c82a7900fedd69e6d7a8972de1c8
MariuszMalec <mario106c@gmail.com>
2023-08-19 19:25:12 +0200

    add unit test roleservice for logic

---
+++ MainApp.BLL.Tests/RoleServiceTests/ApplicationRoleSeedDataFixture.cs
@@ -0,0 +1,53 @@
+﻿using MainApp.BLL.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MainApp_BLL.Tests.RoleServiceTests
+{
+    public class ApplicationRoleSeedDataFixture : IDisposable
+    {
+        public ApplicationDbContext RoleContext { get; private set; }
+
+        public ApplicationRoleSeedDataFixture()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase("RolesDb")
+                .Options;
+
+            RoleContext = new ApplicationDbContext(options);
+
+            RoleContext.Roles.Add(new ApplicationRoles
+            {
+                Id = 1,
+                Name = "SuperAdmin",
+                NormalizedName = "SUPERADMIN"
+            });
+            RoleContext.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            RoleContext.Database.EnsureDeleted();
+            RoleContext.Dispose();
+        }
+
+        public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRoles, int>
+        {
+            public override DbSet<ApplicationRoles> Roles { get; set; }
+
+            public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+                : base(options)
+            {
+            }
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                //optionsBuilder.UseSqlite("Data Source=C:\\Temp\\Databases\\ApplicationUsers.db");//TODO to samo w appsettings.json jest
+            }
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<ApplicationRoles>().ToTable("ApplicationRoles");
+                base.OnModelCreating(modelBuilder);
+            }
+        }
+    }
+}
---
+++ MainApp.BLL.Tests/RoleServiceTests/RoleServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+
+namespace MainApp_BLL.Tests.RoleServiceTests
+{
+    public class RoleServiceTests : IClassFixture<ApplicationRoleSeedDataFixture>
+    {
+        ApplicationRoleSeedDataFixture _fixture;
+        public RoleServiceTests(ApplicationRoleSeedDataFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public void GetRoles_ShoudReturnRoles_WhenExist()
+        {
+            // Arrange
+            var roles = _fixture.RoleContext.Roles;
+
+            // Act
+            var result = roles.Count();
+
+            // Assert
+            result.Should().BeGreaterThan(0);
+        }
+    }
+}
