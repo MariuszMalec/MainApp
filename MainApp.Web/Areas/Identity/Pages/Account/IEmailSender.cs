1123ae05591b4403155ada562ef32e67ca416cd3
MariuszMalec <mario106c@gmail.com>
2023-12-01 16:38:43 +0100

    add all scafolded

---
+++ MainApp.Web/Areas/Identity/Pages/Account/IEmailSender.cs
@@ -0,0 +1,11 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+using System.Threading.Tasks;
+
+namespace MainApp.Web.Areas.Identity.Pages.Account
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string htmlMessage);
+    }
+}
